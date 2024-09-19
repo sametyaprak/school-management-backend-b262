35abbb4a5bac7337bfd292eb978e8e84a9161d1f
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2024-09-19 18:57:58 +0200

    first commit in git session

--- src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
+++ src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
@@ -9,6 +9,9 @@ public class SchoolManagementApplication {
   public static void main(String[] args) {
         SpringApplication.run(SchoolManagementApplication.class, args);
     //test
+    //test-2
+    //test-3
+    //test-4
     }       
     
     
---
+++ src/main/java/com/project/schoolmanagment/Test.java
@@ -0,0 +1,5 @@
+package com.project.schoolmanagment;
+
+public class Test {
+
+}

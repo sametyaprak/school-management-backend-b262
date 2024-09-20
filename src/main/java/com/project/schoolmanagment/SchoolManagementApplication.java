ecaceb2d68eacdad0b68df0d5bf8f3c659e88bcd
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2024-09-20 18:27:34 +0200

    project enums implementation

--- src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
+++ src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
@@ -10,6 +10,23 @@ public class SchoolManagementApplication {
         SpringApplication.run(SchoolManagementApplication.class, args);
     }       
     
+    //admin
+    //dean
+    //vice-dean
+    //student
+    //teacher
+    
+    // education term -> fall + spring
+    // lesson programs -> fall + (monday + 08:00 - 12:00 + java) * M
+    // lesson -> java + css
+    // meetings -> one teacher + many students
+    // student-info -> will be created by teacher for A student
+  
+    // around 65  end-points
+    // around 6000 lines of java code
+    // more than 100 java classes
+    // 10+ entity
+    
     
 }
 
---
+++ src/main/java/com/project/schoolmanagment/entity/enums/RoleType.java
@@ -0,0 +1,16 @@
+package com.project.schoolmanagment.entity.enums;
+
+public enum RoleType {
+  
+  ADMIN("Admin"),
+  TEACHER("Teacher"),
+  STUDENT("Student"),
+  MANAGER("Dean"),
+  ASSISTANT_MANAGER("ViceDean");
+  
+  public final String name;
+
+  RoleType(String name) {
+    this.name = name;
+  }
+}

0c34979aed5524f4a575d27dd60c41bb8833afff
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2024-09-21 18:49:28 +0200

    end of session

--- src/main/java/com/project/schoolmanagment/payload/mappers/UserMapper.java
+++ src/main/java/com/project/schoolmanagment/payload/mappers/UserMapper.java
@@ -1,10 +1,43 @@
 package com.project.schoolmanagment.payload.mappers;
 
+import com.project.schoolmanagment.entity.concretes.user.User;
+import com.project.schoolmanagment.entity.enums.RoleType;
+import com.project.schoolmanagment.payload.request.user.UserRequest;
+import com.project.schoolmanagment.service.user.UserRoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UserMapper {
+  
+  private final UserRoleService userRoleService;
+  
+  public User mapUserRequestToUser(UserRequest userRequest, String userRole) {
+    User user = User.builder()
+        .username(userRequest.getUsername())
+        .name(userRequest.getName())
+        .surname(userRequest.getSurname())
+        .password(userRequest.getPassword())
+        .ssn(userRequest.getSsn())
+        .birthday(userRequest.getBirthDay())
+        .phoneNumber(userRequest.getPhoneNumber())
+        .gender(userRequest.getGender())
+        .email(userRequest.getEmail())
+        .builtIn(userRequest.getBuildIn())
+        .isAdvisor(false)
+        .build();
+    //role handling with switch-case
+    switch (userRole.toLowerCase()) {
+      case "admin":
+        if("Admin".equals(userRequest.getUsername())) {
+          user.setBuiltIn(true);
+        }
+        user.setUserRole(userRoleService.getUserRole(RoleType.ADMIN));
+        break;
+      case "dean":
+        user.setUserRole(userRoleService.getUserRole(RoleType.MANAGER));
+    }
+  }
 
 }
--- src/main/java/com/project/schoolmanagment/service/user/UserRoleService.java
+++ src/main/java/com/project/schoolmanagment/service/user/UserRoleService.java
@@ -2,7 +2,10 @@ package com.project.schoolmanagment.service.user;
 
 import com.project.schoolmanagment.entity.concretes.user.UserRole;
 import com.project.schoolmanagment.entity.enums.RoleType;
+import com.project.schoolmanagment.exception.ResourceNotFoundException;
+import com.project.schoolmanagment.payload.messages.ErrorMessages;
 import com.project.schoolmanagment.repository.user.UserRoleRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,11 @@ public class UserRoleService {
   
   public UserRole getUserRole(RoleType roleType){
     return userRoleRepository.findByEnumRoleEquals(roleType)
-        .orElseThrow(() -> new Reso("User Role Not Found"));
+        .orElseThrow(() -> new ResourceNotFoundException(ErrorMessages.ROLE_NOT_FOUND));
   }
+  
+  public List<UserRole> getAllUserRoles(){
+    return userRoleRepository.findAll();
+  }
+  
 }

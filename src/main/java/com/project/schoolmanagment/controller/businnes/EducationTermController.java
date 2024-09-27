f426068beb14129616aa134c739cd00c284aa2bc
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2024-09-27 20:20:43 +0200

    update EducationTerm

--- src/main/java/com/project/schoolmanagment/controller/businnes/EducationTermController.java
+++ src/main/java/com/project/schoolmanagment/controller/businnes/EducationTermController.java
@@ -7,7 +7,10 @@ import com.project.schoolmanagment.service.businnes.EducationTermService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,14 @@ public class EducationTermController {
   public ResponseMessage<EducationTermResponse>saveEducationTerm(
       @Valid @RequestBody EducationTermRequest educationTermRequest) {
     return educationTermService.saveEducationTerm(educationTermRequest);
+  }
+
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+  @PutMapping("/update/{id}")
+  public ResponseMessage<EducationTermResponse>updateEducationTerm(
+      @PathVariable Long id,
+      @RequestBody EducationTermRequest educationTermRequest){
+    return educationTermService.updateEducationTerm(educationTermRequest,id);
   }
 
 }
--- src/main/java/com/project/schoolmanagment/service/businnes/EducationTermService.java
+++ src/main/java/com/project/schoolmanagment/service/businnes/EducationTermService.java
@@ -3,12 +3,16 @@ package com.project.schoolmanagment.service.businnes;
 import com.project.schoolmanagment.entity.concretes.business.EducationTerm;
 import com.project.schoolmanagment.exception.BadRequestException;
 import com.project.schoolmanagment.exception.ConfictException;
+import com.project.schoolmanagment.exception.ResourceNotFoundException;
+import com.project.schoolmanagment.payload.mappers.EducationTermMapper;
 import com.project.schoolmanagment.payload.messages.ErrorMessages;
+import com.project.schoolmanagment.payload.messages.SuccessMessages;
 import com.project.schoolmanagment.payload.request.businnes.EducationTermRequest;
 import com.project.schoolmanagment.payload.response.businnes.EducationTermResponse;
 import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
 import com.project.schoolmanagment.repository.businnes.EducationTermRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,12 +20,21 @@ import org.springframework.stereotype.Service;
 public class EducationTermService {
   
   private final EducationTermRepository educationTermRepository;
+  private final EducationTermMapper educationTermMapper;
 
   public ResponseMessage<EducationTermResponse> saveEducationTerm(
       EducationTermRequest educationTermRequest) {
     //validation
     validateEducationTermDates(educationTermRequest);
+    //mapping
+    EducationTerm educationTerm = educationTermMapper.mapEducationTermRequestToEducationTerm(educationTermRequest);
+    EducationTerm savedEducationTerm = educationTermRepository.save(educationTerm);
     
+    return ResponseMessage.<EducationTermResponse>builder()
+        .message(SuccessMessages.EDUCATION_TERM_SAVE)
+        .returnBody(educationTermMapper.mapEducationTermToEducationTermResponse(savedEducationTerm))
+        .httpStatus(HttpStatus.CREATED)
+        .build();
   }
   
   private void validateEducationTermDates(EducationTermRequest educationTermRequest) {
@@ -34,7 +47,7 @@ public class EducationTermService {
     //validate not to have any conflict with other education terms
     educationTermRepository.findByYear(educationTermRequest.getStartDate().getYear())
         .forEach(educationTerm -> {
-          if (educationTerm.getStartDate().isAfter(educationTermRequest.getEndDate())
+          if (!educationTerm.getStartDate().isAfter(educationTermRequest.getEndDate())
               || educationTerm.getEndDate().isBefore(educationTermRequest.getStartDate())) {
             throw new BadRequestException(ErrorMessages.EDUCATION_TERM_CONFLICT_MESSAGE);
           }          
@@ -49,10 +62,34 @@ public class EducationTermService {
       throw new ConfictException(ErrorMessages.EDUCATION_START_DATE_IS_EARLIER_THAN_LAST_REGISTRATION_DATE);
     }
     //end>start
-    if(educationTermRequest.getEndDate().isAfter(educationTermRequest.getStartDate())){
+    if(educationTermRequest.getEndDate().isBefore(educationTermRequest.getStartDate())){
       throw new ConfictException(ErrorMessages.EDUCATION_END_DATE_IS_EARLIER_THAN_START_DATE);
     }
   }
+
+
+  public ResponseMessage<EducationTermResponse> updateEducationTerm(
+      EducationTermRequest educationTermRequest, Long id) {
+    //check if education term exists
+    isEducationTermExist(id);
+    //validate dates
+    validateEducationTermDatesForRequest(educationTermRequest);
+    //map
+    EducationTerm term = educationTermMapper.mapEducationTermRequestToEducationTerm(educationTermRequest);
+    term.setId(term.getId());
+    //save to DB
+    EducationTerm savedEducationTerm = educationTermRepository.save(term);
+    //return by mapping it to DTO
+    return ResponseMessage.<EducationTermResponse>builder()
+        .message(SuccessMessages.EDUCATION_TERM_UPDATE)
+        .returnBody(educationTermMapper.mapEducationTermToEducationTermResponse(savedEducationTerm))
+        .httpStatus(HttpStatus.OK)
+        .build();
+  }
   
   
+  public EducationTerm isEducationTermExist(Long id){
+    return educationTermRepository.findById(id)
+        .orElseThrow(()->new ResourceNotFoundException(String.format(ErrorMessages.EDUCATION_TERM_NOT_FOUND_MESSAGE,id)));
+  }
 }

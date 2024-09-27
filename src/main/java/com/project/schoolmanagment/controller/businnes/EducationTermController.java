cc9a4b7e3963f92eac6de0431422cce0fb93ecec
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2024-09-27 20:43:15 +0200

    end of session

--- src/main/java/com/project/schoolmanagment/controller/businnes/EducationTermController.java
+++ src/main/java/com/project/schoolmanagment/controller/businnes/EducationTermController.java
@@ -4,15 +4,21 @@ import com.project.schoolmanagment.payload.request.businnes.EducationTermRequest
 import com.project.schoolmanagment.payload.response.businnes.EducationTermResponse;
 import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
 import com.project.schoolmanagment.service.businnes.EducationTermService;
+import com.sun.xml.bind.v2.TODO;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,6 +41,43 @@ public class EducationTermController {
       @PathVariable Long id,
       @RequestBody EducationTermRequest educationTermRequest){
     return educationTermService.updateEducationTerm(educationTermRequest,id);
+  }
+  
+  //TODO HULYA
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+  @GetMapping("/getAll")
+  public List<EducationTermResponse> getAllEducationTerms(){
+    //return educationTermService.getAllEducationTerms();
+    return null;
+  }
+  
+
+  //TODO HULYA
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+  @GetMapping("/{id}")
+  public EducationTermResponse getEducationTermById(@PathVariable Long id){
+    //return educationTermService.findById(id);
+    return null;
+  }
+
+  //TODO URANUS
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+  @GetMapping("/getAllEducationTermsByPage")
+  public Page<EducationTermResponse> getAllEducationTermsByPage(
+      @RequestParam(value="page",defaultValue= "0") int page,
+      @RequestParam (value = "size", defaultValue = "10") int size,
+      @RequestParam (value = "sort", defaultValue = "name") String sort,
+      @RequestParam (value = "type", defaultValue = "desc") String type){
+    //return educationTermService.getAllByPage(page,size,sort,type);
+    return null;
+  }
+
+  //TODO RUMEYSA
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+  @DeleteMapping("/delete/{id}")
+  public ResponseMessage deleteEducationTerm(@PathVariable Long id){
+    //return educationTermService.deleteById(id);
+    return null;
   }
 
 }

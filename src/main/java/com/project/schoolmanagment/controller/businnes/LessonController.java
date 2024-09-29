98c8267d7dcdbde0a58e5181b1e8e6c5478b78f7
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2024-09-29 18:00:42 +0200

    update lesson program

--- src/main/java/com/project/schoolmanagment/controller/businnes/LessonController.java
+++ src/main/java/com/project/schoolmanagment/controller/businnes/LessonController.java
@@ -1,12 +1,17 @@
 package com.project.schoolmanagment.controller.businnes;
 
+import com.project.schoolmanagment.payload.mappers.LessonMapper;
 import com.project.schoolmanagment.payload.request.businnes.LessonRequest;
 import com.project.schoolmanagment.payload.response.businnes.LessonResponse;
 import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
 import com.project.schoolmanagment.service.businnes.LessonService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class LessonController {
   
   private final LessonService lessonService;
-  
+
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
   @PostMapping("/save")
   public ResponseMessage<LessonResponse>saveLesson(
       @RequestBody @Valid LessonRequest lessonRequest) {
     return lessonService.saveLesson(lessonRequest);
+  }
+
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
+  @PutMapping("/update/{lessonId}")
+  public ResponseEntity<LessonResponse>updateLessonById(
+      @PathVariable Long lessonId,
+      @RequestBody @Valid LessonRequest lessonRequest){
+    return ResponseEntity.ok(lessonService.updateLesson(lessonId,lessonRequest));
   }
 
 }

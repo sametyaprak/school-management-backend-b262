24a78ea9adac0fb1503b3e6bce60c6fd228b630d
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2024-10-03 16:29:33 +0200

    end of student session

--- src/main/java/com/project/schoolmanagment/controller/user/StudentController.java
+++ src/main/java/com/project/schoolmanagment/controller/user/StudentController.java
@@ -1,6 +1,5 @@
 package com.project.schoolmanagment.controller.user;
 
-import com.project.schoolmanagment.payload.request.businnes.AddLessonProgram;
 import com.project.schoolmanagment.payload.request.businnes.AddLessonProgramForStudent;
 import com.project.schoolmanagment.payload.request.user.StudentRequest;
 import com.project.schoolmanagment.payload.request.user.StudentUpdateRequest;
@@ -12,7 +11,6 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
